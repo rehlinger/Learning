a528ca6403d51882e48ab2e45f10521e929653be
rehlinger <rehlinger@outlook.com>
2018-06-29 00:52:38 -0400

    Update

--- generalJS.js
+++ generalJS.js
@@ -5,3 +5,6 @@ let arr3 = Array(5).fill(0)
 let arr4 = Array.apply(null, Array(5)).map(x=>0);
 let arr5 = "?".repeat(5).split("").map(x=>0)
 let arr6 = [...Array(5)].map(x=>0)
+
+arr7 = [2,3,5,4,23,32,22,21,1,1,2,]
+let maxInArray = Math.max(...arr7)//.apply(null, arr7)
