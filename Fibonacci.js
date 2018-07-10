e6cdc6bb49548e71ded26f103ab252a0c33b3161
rehlinger <rehlinger@outlook.com>
2018-07-10 14:25:41 -0400

    Notes

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,3 +1,7 @@
+//Different approaches to solving for the n-th term in a Fibonacci sequence. Solutions are overflow safe for all real numbers.
+//Solutions to avoid unnecessary rescursion to allow for fast solutions to the problem.
+
+//Solution 1 implements a call stack that provides an object for each given number that remembers past history.
 function fib1(n) {
     function fib(n) {
         if(n<=0) return {value: 0};
@@ -10,9 +14,12 @@ function fib1(n) {
 
 console.log(fib1(500));
 
+//Solution 2 implements a memoizing approach that utilizes an array as a lookup table for values to avoid
+//unnecessary computation.
 let fib2 = function (){
     var memo = [0,1];
     var fib = function (n){
+        if (n<0) return 0
         var res = memo[n];
         if (typeof res !== 'number') {res=fib(n-1)+fib(n-2);memo[n]=res;}
         return res;
@@ -22,10 +29,11 @@ let fib2 = function (){
 
 console.log(fib2(500))
 
+//Solutions 3 implements an fixed sized array (length 3) that is continuously updated as the stack is computed from 0->n.
 let fib3 = function(){
     let arr = Array(3).fill(0);
     let fib = function (n) {
-        if (n===0) return 0;
+        if (n<=0) return 0;
         if (n===1) {arr.pop();arr.unshift(1);return;}
         fib(n-1);
         arr.pop();
